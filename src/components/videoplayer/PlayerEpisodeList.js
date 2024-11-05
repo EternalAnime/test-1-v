29f85ff5f561d437f60e43fde97d16bd61a8ef48
EternalAnime <eternalmelodymagic@gmail.com>
2024-11-05 11:18:30 +0530

    Update PlayerEpisodeList.js

--- src/components/videoplayer/PlayerEpisodeList.js
+++ src/components/videoplayer/PlayerEpisodeList.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import styles from '../../styles/PlayerEpisodeList.module.css'
-import { getEpisodes } from "@/lib/getData";
+import { getEpisodes } from "@/actions/episode";
 import { ProvidersMap } from "@/utils/EpisodeFunctions";
 import { useRouter } from 'next-nprogress-bar';
 import EpImgContent from "../Episodelists/EpImgContent";
@@ -110,7 +110,7 @@ function PlayerEpisodeList({ id, data, onprovider, setwatchepdata, epnum }) {
     if (!providerChanged && (currentEpisodes?.[epnum - 1]?.id || currentEpisodes?.[epnum - 1]?.episodeId)) {
       const episodeId = encodeURIComponent(currentEpisodes?.[epnum - 1]?.id || currentEpisodes?.[epnum - 1]?.episodeId);
       router.push(`/anime/watch?id=${id}&host=${defaultProvider}&epid=${episodeId}&ep=${epnum}&type=${subtype}`);
-    }  
+    }
   //   setTimeout(() => {
   // }, 0);
   }, [providerChanged]);
@@ -163,7 +163,7 @@ function PlayerEpisodeList({ id, data, onprovider, setwatchepdata, epnum }) {
                   SUB: </span>
                 {episodeData?.map((item, index) => (
                   <div key={item.providerId} value={item.providerId} className={item.providerId === defaultProvider && subtype === 'sub' ? styles.providerselected : styles.provider} onClick={() => handleProviderChange(item.providerId, "sub")}>
-                    HD-{index+1}
+                    {item.providerId}
                   </div>
                 ))}
               </div>
@@ -175,7 +175,7 @@ function PlayerEpisodeList({ id, data, onprovider, setwatchepdata, epnum }) {
                   DUB: </span>
                 {episodeData?.map((item, index) => (
                   <div key={item.providerId} value={item.providerId} className={item.providerId === defaultProvider && subtype === 'dub' ? styles.providerselected : styles.provider} onClick={() => handleProviderChange(item.providerId, "dub")}>
-                    HD-{index+1}
+                   {item.providerId}
                   </div>
                 ))}
               </div>
@@ -246,7 +246,7 @@ function PlayerEpisodeList({ id, data, onprovider, setwatchepdata, epnum }) {
                       className={` cursor-pointer ${eplisttype === 2 ? 'selected' : ''}`}
                       onClick={() => handleOptionClick(2)}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={`${eplisttype === 2 ? '#6E00FF' : 'currentColor'}`} className="w-6 h-6">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={`${eplisttype === 2 ? '#ca1313' : 'currentColor'}`} className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
                       </svg>
 
@@ -255,7 +255,7 @@ function PlayerEpisodeList({ id, data, onprovider, setwatchepdata, epnum }) {
                       className={` cursor-pointer ${eplisttype === 3 ? 'selected' : ''}`}
                       onClick={() => handleOptionClick(3)}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={`${eplisttype === 3 ? '#6E00FF' : 'currentColor'}`} className="w-6 h-6">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={`${eplisttype === 3 ? '#ca1313' : 'currentColor'}`} className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
                       </svg>
                     </span>
